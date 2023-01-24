df962e11e9e9165484d929d89df6745f4895bdf6
Prajwall64 <36110077+Prajwall64@users.noreply.github.com>
2023-01-23 19:35:38 -0800

    main cunt

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+if values == int(-1):
+    gay = False
---
+++ test.py
@@ -0,0 +1,10 @@
+
+gay = True
+
+while gay:
+    values = int(input("Enter a value: "))
+
+
+if values == -1:
+    gay = False
+    print(gay)
